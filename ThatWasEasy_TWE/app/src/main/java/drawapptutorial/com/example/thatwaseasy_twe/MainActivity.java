98e42e9691b8cad19dac2c9f59677920592107e8
ccoreil14 <ccoreil14@gmail.com>
2017-02-23 21:43:34 -0700

    Added two now layouts for dialog boxes and add task button makes pop up (no add functionality yet)

--- ThatWasEasy_TWE/app/src/main/java/drawapptutorial/com/example/thatwaseasy_twe/MainActivity.java
+++ ThatWasEasy_TWE/app/src/main/java/drawapptutorial/com/example/thatwaseasy_twe/MainActivity.java
@@ -1,13 +1,66 @@
 package drawapptutorial.com.example.thatwaseasy_twe;
 
+import android.app.Dialog;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
+
+    private Dialog myDialog;
+    private EditText nameField;
+    private EditText descField;
+    private EditText minuteField;
+    private Button addTaskBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addTaskBtn = (Button) findViewById(R.id.addBtn);
+        addTaskBtn.setOnClickListener(this);
+        addTaskBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                callAddTaskDialog();
+            }
+        });
+    }
+
+
+
+
+    private void callAddTaskDialog()
+    {
+        myDialog = new Dialog(this);
+        myDialog.setContentView(R.layout.add_task_layout);
+        myDialog.setCancelable(false);
+
+        nameField = (EditText) myDialog.findViewById(R.id.nameField);
+        descField = (EditText) myDialog.findViewById(R.id.descField);
+        minuteField = (EditText) myDialog.findViewById(R.id.minuteField);
+        myDialog.show();
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
