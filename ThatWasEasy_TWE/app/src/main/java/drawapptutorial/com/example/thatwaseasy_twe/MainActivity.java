f5bebfb0f29cbb36c74c71c926582ae63d54e4aa
ccoreil14 <ccoreil14@gmail.com>
2017-02-25 13:58:40 -0700

    made the update_task_layout.xml but no functionality attached yet but can do that easily. Fixed a huge bug with dialog where it was not being initialized on create so I had to move the code that was in an onclick into the on create function. Add task dialog adds to the database with all info. The timer time and is complete is set to a default of "Not complete" and 0

--- ThatWasEasy_TWE/app/src/main/java/drawapptutorial/com/example/thatwaseasy_twe/MainActivity.java
+++ ThatWasEasy_TWE/app/src/main/java/drawapptutorial/com/example/thatwaseasy_twe/MainActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.List;
 
@@ -19,55 +20,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private EditText nameField;
     private EditText descField;
     private EditText minuteField;
+    private Spinner urgencyField;
+    private Button openAddTaskDialogBtn;
     private Button addTaskBtn;
+    private Button updateTaskBtn;
+    DBHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        DBHandler db = new DBHandler(this);
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        addTaskBtn = (Button) findViewById(R.id.addBtn);
-        addTaskBtn.setOnClickListener(this);
-        addTaskBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                callAddTaskDialog();
-            }
-        });
-
-        Log.d("Insert: ", "Inserting ..");
-        db.addTask(new Task(3, "Make Test Tasks", "I need to make a test task to make sure all the functions are all working appropriately", "High", "Not Complete", 0));
-        db.addTask(new Task(1, "Run Test", "Run test to make sure all the functions are all working appropriately", "High", "Not Complete", 0));
-
-
-        Log.d("Reading: ", "Reading all shops after adding..");
-        List<Task> tasks= db.getAllTasks();
-
-        for (Task task: tasks) {
-            String log = "Id: " + task.getId() + " ,Name: " + task.getName() + " ,Description: " + task.getDesc() + " ,Estimated Minutes to complete: " + task.getMinutes() + " ,Urgency: " + task.getUrg() + " ,Is it Completed?: " + task.getCompletion() + " ,Current Timer Num: " + task.getTimerNum();
-            // Writing shops to log
-            Log.d("Task: : ", log);
-        }
-
-        for(int i = 1; i <=2; i++) db.deleteTask(db.getTask(i));
-
-        Log.d("Reading: ", "Reading all shops again after deleting..");
-        tasks = db.getAllTasks();
-
-        for (Task task: tasks) {
-            String log = "Id: " + task.getId() + " ,Name: " + task.getName() + " ,Description: " + task.getDesc() + " ,Estimated Minutes to complete: " + task.getMinutes() + " ,Urgency: " + task.getUrg() + " ,Is it Completed?: " + task.getCompletion() + " ,Current Timer Num: " + task.getTimerNum();
-            // Writing shops to log
-            Log.d("Task Yo: : ", log);
-        }
-    }
-
-
-
-
-    private void callAddTaskDialog()
-    {
+        db = new DBHandler(this);
         myDialog = new Dialog(this);
         myDialog.setContentView(R.layout.add_task_layout);
         myDialog.setCancelable(false);
@@ -75,9 +36,79 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         nameField = (EditText) myDialog.findViewById(R.id.nameField);
         descField = (EditText) myDialog.findViewById(R.id.descField);
         minuteField = (EditText) myDialog.findViewById(R.id.minuteField);
+        urgencyField = (Spinner) myDialog.findViewById(R.id.urgencySpinner);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        openAddTaskDialogBtn = (Button) findViewById(R.id.addBtn);
+        openAddTaskDialogBtn.setOnClickListener(this);
+        openAddTaskDialogBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                callAddTaskDialog();
+            }
+        });
+
+        addTaskBtn = (Button) myDialog.findViewById(R.id.addTaskBtn);
+        addTaskBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                addTaskFromForm(Integer.parseInt( minuteField.getText().toString()), nameField.getText().toString(), descField.getText().toString(), urgencyField.getSelectedItem().toString());
+                seeListInConsole();
+                myDialog.dismiss();
+            }
+        });
+
+
+        
+//        Log.d("Insert: ", "Inserting ..");
+//        db.addTask(new Task(3, "Make Test Tasks", "I need to make a test task to make sure all the functions are all working appropriately", "High", "Not Complete", 0));
+//        db.addTask(new Task(1, "Run Test", "Run test to make sure all the functions are all working appropriately", "High", "Not Complete", 0));
+//
+//
+//        Log.d("Reading: ", "Reading all shops after adding..");
+//        List<Task> tasks= db.getAllTasks();
+//
+//        for (Task task: tasks) {
+//            String log = "Id: " + task.getId() + " ,Name: " + task.getName() + " ,Description: " + task.getDesc() + " ,Estimated Minutes to complete: " + task.getMinutes() + " ,Urgency: " + task.getUrg() + " ,Is it Completed?: " + task.getCompletion() + " ,Current Timer Num: " + task.getTimerNum();
+//            // Writing shops to log
+//            Log.d("Task: : ", log);
+//        }
+//
+//        for(int i = 1; i <=2; i++) db.deleteTask(db.getTask(i));
+//
+//        Log.d("Reading: ", "Reading all shops again after deleting..");
+//        tasks = db.getAllTasks();
+//
+//        for (Task task: tasks) {
+//            String log = "Id: " + task.getId() + " ,Name: " + task.getName() + " ,Description: " + task.getDesc() + " ,Estimated Minutes to complete: " + task.getMinutes() + " ,Urgency: " + task.getUrg() + " ,Is it Completed?: " + task.getCompletion() + " ,Current Timer Num: " + task.getTimerNum();
+//            // Writing shops to log
+//            Log.d("Task Yo: : ", log);
+//        }
+    }
+
+
+
+
+    private void callAddTaskDialog()
+    {
+
         myDialog.show();
 
+    }
 
+    private void addTaskFromForm(int minutesNum, String taskName, String taskDesc, String urgencyType){
+        db.addTask(new Task(minutesNum, taskName, taskDesc, urgencyType, "Not Complete", 0));
+    }
+
+    private void seeListInConsole(){
+        List<Task> tasks= db.getAllTasks();
+
+        for (Task task: tasks) {
+            String log = "Id: " + task.getId() + " ,Name: " + task.getName() + " ,Description: " + task.getDesc() + " ,Estimated Minutes to complete: " + task.getMinutes() + " ,Urgency: " + task.getUrg() + " ,Is it Completed?: " + task.getCompletion() + " ,Current Timer Num: " + task.getTimerNum();
+            // Writing shops to log
+            Log.d("Task Yo: : ", log);
+        }
     }
 
     @Override
